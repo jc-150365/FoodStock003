6dfe006ae289be7ec66f0d093f9071c37258f20f
謙太 可士村 <150365@jc-21.jp>
2018-01-09 15:29:41 +0900

    メモ色変更

--- FoodStock01/FoodStock01/MemoPage.cs
+++ FoodStock01/FoodStock01/MemoPage.cs
@@ -88,7 +88,7 @@ namespace FoodStock01
                     {
                         new StackLayout
                         {
-                            BackgroundColor = Color.HotPink,
+                            BackgroundColor = Color.Green,
                             Padding = 5,
                             Orientation = StackOrientation.Horizontal,
                             Children = {entry,buttonAdd}//Entryコントロールとボタンコントロールを配置
